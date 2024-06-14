73bac197d77afc90ed5acccc70386a8d94c7e610
JvLetsBora <jv.oliveira.designer@gmail.com>
2024-06-14 10:11:48 -0300

    ...

--- app/app/main.py
+++ app/app/main.py
@@ -1,50 +1,97 @@
 import sys
 import uvicorn
 from fastapi import FastAPI, HTTPException
-from config.db import Migratinios
-from models import pedidos
-
-migrations = Migratinios()
+import logging
+from pydantic import BaseModel
 
 
 app = FastAPI(docs_url="/api/docs")
 
+blog_posts = []
 
-@app.post("/migratinios")
-async def root():
-    response = await migrations.make_migrations()
-    return {"Resposta": response}
 
-@app.post("/pedido/novo")
-async def create_pedido_for_user(
-    pedido: pedidos.PedidoCreate
-):
-    return await pedidos.create_pedido(pedido=pedido)
+logging.basicConfig(filename='logs/app.log', level=logging.WARNING, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
 
-@app.get("/pedido/pedidos/{id}")
-async def create_pedido_for_user(
+class BlogPost:
+    def __init__(self, id, title, content):
+        self.id = id
+        self.title = title
+        self.content = content
+    def __str__(self) -> str:
+        return f'{self.id} - {self.title} - {self.content}'
+    
+    def toJson(self):
+        return {'id': self.id, 'title': self.title, 'content': self.content}
+
+class PostCreated(BaseModel):
     id: int
-):
-    return await pedidos.get_pedido_id(id=id)
+    title: str
+    content: str
+
+@app.post('/blog', status_code=201)
+def create_blog_post(request:PostCreated):
+    try:
+        data = request
+        blog_posts.append(BlogPost(data.id, data.title, data.content))
+        logger.info("created new post")
+        return {'status':'sucess'}
+    except KeyError:
+        logger.error("Invalid request")
+        raise HTTPException(status_code=400, detail={'error': 'Invalid request'})
+    except Exception as e:
+        logger.warning(str(e))
+        raise HTTPException(status_code=500, detail={'error': str(e)})
+        
 
 
-@app.get("/pedido/pedidos")
-async def tasks():
-    tasks = await pedidos.get_pedidos()
-    return tasks
+@app.get('/blog', status_code=200)
+def get_blog_posts():
+    logger.info("get all posts")
+    return {'posts': [blog.toJson() for blog in blog_posts]}
 
-@app.delete("/pedido/pedidos/{id}")
-async def create_pedido_for_user(
-    id: int
-):
-    return await pedidos.delete_pedido(id=id)
 
-@app.put("/pedido/pedidos/{id}")
-async def create_pedido_for_user(
-    id:int,
-    pedido: pedidos.PedidoBase
-):
-    return await pedidos.update_pedido(id=id, pedido=pedido)
+@app.get('/blog/{id}', status_code=200)
+def get_blog_post(id:int):
+    for post in blog_posts:
+        if post.id == id:
+            logger.info(f"get post {id}")
+            return {'post': post.__dict__}
+    logger.error(f"post {id} not found")
+    raise HTTPException(status_code=404, detail={'error': 'Post not found'})
+    
+
+@app.delete('/blog/{id}', status_code=200)
+def delete_blog_post(id:int):
+    for post in blog_posts:
+        if post.id == id:
+            blog_posts.remove(post)
+            logger.info(f"delete post {id}")
+            return {'status':'sucess'}
+    logger.error(f"post {id} not found")
+    raise HTTPException(status_code=404, detail={'error': 'Post not found'})
+    
+
+@app.put('/blog/{id}', status_code=200)
+def update_blog_post(id:int, request:PostCreated):
+    try:
+        data = request
+        for post in blog_posts:
+            if post.id == id:
+                post.title = data.title
+                post.content = data.content
+                logger.info(f"update post {id}")
+                return {'status':'sucess'}
+        logger.error(f"post {id} not found")
+        raise HTTPException(status_code=404, detail={'error': 'Post not found'})
+    except KeyError:
+        logger.error("Invalid request")
+        raise HTTPException(status_code=400, detail={'error': 'Invalid request'})
+    except Exception as e:
+        logger.warning(str(e))
+        raise HTTPException(status_code=500, detail={'error': str(e)})
+        
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001)
